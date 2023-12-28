28237efc93c2444da90ab57d7c94a16a8aa2ea46
deyul <liderrrrrr0310@163.com>
2023-12-28 18:58:33 +0800

    final

--- love/cyber.js
+++ love/cyber.js
@@ -1,12 +1,4 @@
-/*****
-DONE: Add author to the tweet
-TODO: Fix the 140 character tweet limit
-TODO: Random number/quote on click shouldn't sometimes be the same as the previous one, making the quote form look unresponsive/broken
-TODO: Rotating phone from landscape to portrait causes height issues
-TODO: Give users the ability to add quotes and/or upvote or rate their favorite quotes -- could be abused though.
-TODO: More quotes!
-TODO: Add more cyberpunk-y effects and animations
-*****/
+
 
 $(document).ready(function() {
     var quoteList = [
